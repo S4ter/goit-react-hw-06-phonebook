b8b82b3b9f6b5c36d43b030d4916a7a3d9ce947e
Patryk Stefański <119447102+S4ter@users.noreply.github.com>
2023-08-06 22:28:57 +0200

    code organisation

--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -1,41 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ContactsList.styles.css';
+import { useDispatch } from 'react-redux';
+import { getContacts } from 'redux/selectors';
 
-export const ContactsList = () =>
-  // filter,
-  // onFilterChange,
-  // contacts,
-  // deleteContact,
-  {
-    // const filteredContacts = contacts.filter(contact =>
-    //   contact.name.toLowerCase().includes(filter.toLowerCase())
-    // );
-
-    return (
-      <div className="filter_form">
-        <h2 className="filter_title">Contacts:</h2>
-        <input
-          type="text"
-          name="filter"
-          placeholder="Search..."
-          // value={filter}
-          // onChange={onFilterChange}
-          className="filter_input"
-        />
-        <ul></ul>
-        {/* <ul>
-        {filteredContacts.map(({ name, number }, index) => (
-          <li key={index}>
-            {name}: {number}
-            <button
-              className="delete_button"
-              onClick={() => deleteContact(index)}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul> */}
-      </div>
-    );
+export const ContactsList = () => {
+  const dispatch = useDispatch();
+  const [searchInputValue, setSearchInputValue] = useState('');
+  // const filteredContacts = contacts.filter(contact =>
+  //   contact.name.toLowerCase().includes(filter.toLowerCase())
+  // );
+  const handleSearchInput = e => {
+    e.preventDefault();
+    setSearchInputValue(e.target.value);
   };
+
+  return (
+    <div className="filter_form">
+      <h2 className="filter_title">Contacts:</h2>
+      <input
+        type="text"
+        name="filter"
+        placeholder="Search..."
+        // value={filter}
+        onChange={handleSearchInput}
+        className="filter_input"
+      />
+      <ul></ul>
+      {/* <ul>
+          {filteredContacts.map(({ name, number }, index) => (
+            <li key={index}>
+              {name}: {number}
+              <button
+                className="delete_button"
+                onClick={() => deleteContact(index)}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul> */}
+    </div>
+  );
+};
